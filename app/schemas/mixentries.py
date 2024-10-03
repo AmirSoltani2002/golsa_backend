2fcce9176901dcaa0ed8c4852b180ccde93a5fd9
amirabbas <amir.soltani1381@gmail.com>
2024-10-03 13:00:42 +0330

    edit jdatetime

--- app/schemas/mixentries.py
+++ app/schemas/mixentries.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, validator
-import jdatetime
 from datetime import date as dt
 from datetime import time as tm
 
@@ -13,20 +12,6 @@ class MixEntry(BaseModel):
     time: tm
     date: dt
     fitting: bool
-
-    @validator('date', pre=True)
-    def parse_persian_date(cls, value):
-        try:
-            # Ensure the input value is a string
-            if not isinstance(value, str):
-                value = str(value)
-            # Assume value is in the format 'YYYY-MM-DD' in Persian calendar
-            persian_date = jdatetime.date.fromisoformat(value)
-            # Convert to Gregorian date
-            gregorian_date = persian_date.togregorian()
-            return gregorian_date
-        except Exception as e:
-            raise e
 
 class MixEntryResponse(MixEntry):
     id: int
