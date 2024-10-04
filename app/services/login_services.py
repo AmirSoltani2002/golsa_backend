32bc948df103f6b8773e0a97ddac0a7c61144886
amirabbas <amir.soltani1381@gmail.com>
2024-10-04 19:04:19 +0330

    edit signup

--- app/services/login_services.py
+++ app/services/login_services.py
@@ -6,7 +6,7 @@ from app.schemas.users import User_login
 def Get_user(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
 
-def Create_user(db: Session, user: User_login):
+def Create_user(db: Session, user: dict):
     user = user.dict()
     user = User(**user)
     db.add(user)
