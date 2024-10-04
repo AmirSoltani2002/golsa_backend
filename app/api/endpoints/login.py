e655650d1a4d83542744cf288b93be2d9fb515dd
amirabbas <amir.soltani1381@gmail.com>
2024-10-04 12:27:08 +0330

    debug sign 4

--- app/api/endpoints/login.py
+++ app/api/endpoints/login.py
@@ -27,10 +27,10 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
     access_token = create_access_token(data = {"sub": user.username, "role": user.role})
     return {"access_token": access_token, "type": "bearer", "role": user.role, "username": user.username}
 
-@router.post("/signup/", response_model=user_schemas.User_show)
-def signup(data: user_schemas.User_login, db: Session = Depends(get_db), user = Depends(get_current_user)):
+@router.post("/signup/")
+def signup(data: dict, db: Session = Depends(get_db), user = Depends(get_current_user)):
     if user['role'] != 'admin':
         raise HTTPException(status_code=403, detail="Not enough permission")
-    return 1
-    data.hashed_pass = pwd_context.hash(data.hashed_pass)
+    #return data['hashed_pass']
+    data['hashed_pass'] = pwd_context.hash(data['hashed_pass'])
     return Create_user(db, data)
