286008309c51a7179e8796872cef7fdd13a1b91d
amirabbas <amir.soltani1381@gmail.com>
2024-10-17 19:06:30 +0330

    join table

--- app/api/endpoints/recipes.py
+++ app/api/endpoints/recipes.py
@@ -7,8 +7,6 @@ from app.api.dependencies import get_db, get_current_user
 
 router = APIRouter()
 
-@router.get("/api/recipes/", response_model=List[Recipe])
-def read_materials(db: Session = Depends(get_db), user = Depends(get_current_user)):
-    if user['role'] != 'admin':
-        raise HTTPException(status_code = 403, detail = 'Not enough permission')
+@router.get("/api/recipes/")
+def read_materials(db: Session = Depends(get_db)):
     return Get_recipes(db)
--- app/services/recipes_services.py
+++ app/services/recipes_services.py
@@ -1,4 +1,6 @@
 from app.models.recipes import Recipe
+from app.models.materials import Material
+from app.models.rawmaterials import RawMaterial
 from typing import List
 from sqlalchemy.orm import Session
 
@@ -15,6 +17,26 @@ def Create_recipes(db: Session, recipes: [dict]):
 
 
 def Get_recipes(db: Session):
-    recipes = db.query(Recipe).join(Recipe.rawmaterial).join(Recipe.material).all()
-    return recipes
+    recipes = db.query(
+        Recipe.id, 
+        Material.id, 
+        Material.material, 
+        RawMaterial.id, 
+        RawMaterial.rawmaterial, 
+        RawMaterial.company)\
+            .join(Recipe.rawmaterial)\
+                .join(Recipe.material)\
+    .all()
+    flattened_result = [
+        {
+            "recipe_id": row[0],
+            "material_id": row[1],
+            "material_name": row[2],
+            "rawmaterial_id": row[3],
+            "rawmaterial_name": row[4],
+            "rawmaterial_company": row[5],
+        }
+        for row in recipes
+    ]
+    return flattened_result
 
