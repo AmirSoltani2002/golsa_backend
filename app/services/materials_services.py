e07b994a0d2e3e1ecaf79f24f42caabbd628dfda
amirabbas <amir.soltani1381@gmail.com>
2024-11-13 18:37:03 +0330

    add rawmat

--- app/services/materials_services.py
+++ app/services/materials_services.py
@@ -8,7 +8,7 @@ def Get_materials(db: Session):
     return db.query(Material).filter(Material.material != None).all()
 
 def Get_material_by_id(id: int, db: Session):
-    return db.query(Recipe).filter(Recipe.material_id == id).all()
+    return db.query(Recipe).filter(Recipe.material_id == id and Recipe.rawmaterial_id != None).all()
 
 def Post_material(db: Session, material: str = None):
     db_material = Material(material = material)
