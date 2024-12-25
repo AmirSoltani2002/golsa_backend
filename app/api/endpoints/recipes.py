f5fc76783c3e93553bbf63430012111329c67fbc
amirabbas <amir.soltani1381@gmail.com>
2024-12-25 21:38:11 +0330

    auti

--- app/api/endpoints/recipes.py
+++ app/api/endpoints/recipes.py
@@ -9,6 +9,6 @@ router = APIRouter()
 
 @router.get("/api/recipes/")
 def read_materials(db: Session = Depends(get_db), user = Depends(get_current_user)):
-    if user['role'] != 'admin':
-        raise HTTPException(status_code = 403, detail = 'Not enough permission')
+    # if user['role'] != 'admin':
+    #     raise HTTPException(status_code = 403, detail = 'Not enough permission')
     return Get_recipes(db)
