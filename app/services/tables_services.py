50165e85ad18df43f17fa027a901500fa45afa9e
amirabbas <amirsoltani1381@gmail.com>
2025-03-02 12:48:13 +0330

    get by id

--- app/services/tables_services.py
+++ app/services/tables_services.py
@@ -153,7 +153,7 @@ def Get_by_id(name:str, id: int, db: Session):
                 WHERE id = {id};
                 """)
             ).fetchall()
-        return result
+        return result.to_dict()
     except SQLAlchemyError as e:
         raise HTTPException(status_code = 422, detail = str(e))
 
