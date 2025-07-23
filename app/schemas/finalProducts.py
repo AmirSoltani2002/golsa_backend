e5959333bbf74085dfef78d20bcf7bf5cb167699
amirabbas <amirsoltani1381@gmail.com>
2025-07-23 14:40:46 +0330

    fix: update FinalProduct schema to use mold_id as integer and adjust product_id type

--- app/schemas/finalProducts.py
+++ app/schemas/finalProducts.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel, field_validator
 class FinalProduct(BaseModel):
     id: int | None = None
     line_id: int | None = None
-    mold: str | None = None
-    product_id: str | None = None
+    mold_id: int | None = None
+    product_id: int | None = None
     type: str | None = None
     cycle_tobe: int | None = None
     quantity_tobe: int | None = None
